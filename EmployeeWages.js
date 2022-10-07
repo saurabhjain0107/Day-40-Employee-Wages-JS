6d388f963c7164946f6b76d30669c5f36add862c
saurabh jain <luckyjain0107@gmai.com>
2022-10-07 14:33:35 +0530

    Store Daily Wage With Total Wage

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -13,15 +13,21 @@ switch(empCheck){
         return 0;
     }
 }
-const MAX_HRS_IN_MONTH = 100;
+function calculateDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+  }
+const MAX_HRS_IN_MONTH = 160;
 const NUM_OF_WORKING_DAYS = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
 while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
   totalWorkingDays++;
     let empCheck = Math.floor(Math.random() *10) % 3;
-  totalEmpHrs+= getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs +=empHrs;
+    empDailyWageArr.push(calculateDailyWage(empHrs));
 }
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
+let empWage = calculateDailyWage(totalEmpHrs);
 console.log("Total Days :"+ totalWorkingDays +" Total Hrs: " + 
             totalEmpHrs+ " Employee Wage: "+ empWage)
