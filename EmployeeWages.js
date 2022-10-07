4ff9409f755482bfc3071c949e31482e20590704
saurabh jain <luckyjain0107@gmai.com>
2022-10-07 10:44:48 +0530

    Calculate Wages for Month

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -13,8 +13,11 @@ switch(empCheck){
         return 0;
     }
 }
+const NUM_OF_WORKING_DAYS = 2;
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() *10) % 3;
-empHrs = getWorkingHours(empCheck);
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+  let empCheck = Math.floor(Math.random() *10) % 3;
+empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage: "+ empWage);
+console.log("Total Hrs: " + empHrs + " Employee Wage: "+ empWage);
